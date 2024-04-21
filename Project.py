5b91bd523db6434fa98ba4cfac82e49684579073
aelfatih <abdel.elfatih@gmail.com>
2024-04-21 13:31:58 +0530

    updated on READM and ProjectE

--- Project.py
+++ Project.py
@@ -1,1 +1,23 @@
 # This is the first project for Infra
+
+Git Commands:
+-------------
+git init
+git commit "Add Initial Files."
+
+
+# You are telling Git to associate the specified URL with remote Repository named "origin"
+git remote add origin https://github.com/ammishra08/DockerRepo.git
+
+git remote -v
+git branch
+git branch -M main
+git branch
+git push origin main
+git push -u origin main
+
+# Any update in existing file
+git add .
+git status
+git commit -m "Updated README"
+git push
--- employee.py
+++ employee.py
@@ -27,3 +27,30 @@ print("All Employees: ")
 view_employees()
 
 search_employee("Aayam")
+
+
+# DockerRepo
+
+This is first Git Repository of Docker Repo. This Repository have python projects and database projects.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+Git Commands:
+-------------
+git init
+git commit -m "Add Initial Files."
+
+# You are telling Git to associate the specified URL with remote Repository named "origin"
+git remote add origin https://github.com/ammishra08/DockerRepo.git
+
+git remote -v
+git branch
+git branch -M main
+git branch
+git push origin main
+git push -u origin main
+
+# Any update in existing file
+git add .
+git status
+git commit -m "Updated README"
+git push
